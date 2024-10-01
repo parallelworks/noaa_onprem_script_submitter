d7dfbc1c189cfefaba88075582a150095f4475d4
avidalto <avidalto@hawk.iit.edu>
2024-10-01 06:53:13 -0500

    Improve logs

--- transfer_job_script.sh
+++ transfer_job_script.sh
@@ -3,5 +3,6 @@ source resources/host/inputs.sh
 source load_env.sh
 
 echo; echo; echo "TRANSFER JOB SCRIPT TO CLUSTER"
+set -x
 ${sshcmd} "mkdir -p ${resource_jobdir}"
 scp job_script ${resource_publicIp}:${resource_jobdir}/job_script
