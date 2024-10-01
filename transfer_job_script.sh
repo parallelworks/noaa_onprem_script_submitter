585d228dc50483c31e350da265858d6b92c36e6f
avidalto <avidalto@hawk.iit.edu>
2024-10-01 06:59:27 -0500

    use rsync instead of scp

--- transfer_job_script.sh
+++ transfer_job_script.sh
@@ -4,4 +4,4 @@ source load_env.sh
 
 echo; echo; echo "TRANSFER JOB SCRIPT TO CLUSTER"
 set -x
-rsync -e "${sshcmd}" --rsync-path="mkdir -p ${resource_jobdir} && rsync" job_script ${resource_publicIp}:${resource_jobdir}/job_script
+rsync -avzq  --rsync-path="mkdir -p ${resource_jobdir} && rsync" job_script ${resource_publicIp}:${resource_jobdir}/job_script
